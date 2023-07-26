67b738e665d0b6658ba07d84dac38a166bc0572b
Saendy <jayden_saw@hotmail.com>
2023-07-26 12:13:15 +0800

    exercises 6.1-6.2

--- src/reducer.js
+++ src/reducer.js
@@ -7,17 +7,27 @@ const initialState = {
 const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
-    case 'GOOD':
-      return state
-    case 'OK':
-      return state
-    case 'BAD':
-      return state
+    case 'GOOD': {
+      const newState = { ...state, good: state.good + 1 }
+      return newState
+    }
+    case 'OK': {
+      const newState = { ...state, ok: state.ok + 1 }
+      return newState
+    }
+    case 'BAD': {
+      const newState = { ...state, bad: state.bad + 1 }
+      return newState
+    }
     case 'ZERO':
-      return state
+      return {
+        good: 0,
+        ok: 0,
+        bad: 0
+      }
     default: return state
   }
-  
+
 }
 
 export default counterReducer
